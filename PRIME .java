d7ef0a3541d98f273765a752545597c47fa99cb7
monishat5522-sketch <monisha.t5522@gmail.com>
2026-07-17 15:27:13 +0530

    Create PRIME .java

---
+++ PRIME .java
@@ -0,0 +1,18 @@
+class Main {
+    public static void main(String[] args) {
+        int n =13;
+        String str = prime(14);
+        System.out.println(str);
+      }
+      static String prime (int k ){
+          if(k<2)
+          {
+              return "not prime";
+      }
+      for(int i =2;i<k;i++){
+          if(k%i==0)
+          return "not prime";
+      }
+      return "prime";
+      }
+}
