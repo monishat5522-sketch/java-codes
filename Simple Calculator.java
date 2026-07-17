8923fcc3378ec38a1d439142ccac54bafae3f2f6
monishat5522-sketch <monisha.t5522@gmail.com>
2026-07-17 14:35:25 +0530

    Create Simple Calculator.java

---
+++ Simple Calculator.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number:");
+        int a =sc.nextInt();
+        System.out.print("Enter second number:");
+        int b =sc.nextInt();
+        System.out.println("addition ="+(a+b));
+        System.out.println("subtraction ="+(a+b));
+        System.out.println("multiplication ="+(a+b));
+        System.out.println("division ="+(a+b));
+ }
+}
