4ef1601c4439435345a0cf486d6eb5a65d439cce
MONISHA.T <monisha.t5522@gmail.com>
2026-07-21 14:57:16 +0530

    Create Concatenation.java

---
+++ Concatenation.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        String a = "Elecrical";
+        String b = "And";
+        String c = "Elctronics";
+        System.out.println(a + "" + b + ""+ c);
+    
+    }
+}
