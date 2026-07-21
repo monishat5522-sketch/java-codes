184a9b299f9bbbda7a0cb6238d6ce5b87f4f05e4
MONISHA.T <monisha.t5522@gmail.com>
2026-07-21 14:39:33 +0530

    Create Uppercase and Lowercase.java

---
+++ Uppercase and Lowercase.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        String name = "monisha";
+        
+        // Correct camelCase method names
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+    }
+}
